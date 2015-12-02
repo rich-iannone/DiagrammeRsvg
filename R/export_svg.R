f587508deb7d6f4f68e1245f018f74ee14bc8ff8
Richard Iannone <riannone@me.com>
2015-12-02 09:56:06 -0800

    Modify example in roxygen @examples section

--- R/export_svg.R
+++ R/export_svg.R
@@ -6,8 +6,7 @@
 #' @examples
 #' \dontrun{
 #'  library(DiagrammeR)
-#'  (svg <- exportSVG(grViz('digraph{a->b; c->a; c->b; c->d;}',
-#'    engine = 'circo')))
+#'  svg <- exportSVG(grViz('digraph{a->b; c->a; c->b; c->d;}'))
 #'
 #'  # this can then be used with htmltools and can save significantly
 #'  # on size of output using svg rather than unrendered grViz
